cd7b7f2fdba07b94277c42a9f7e4e324d40213a5
CodeAce36 <paulangelo.derige@gmail.com>
2024-09-17 23:17:46 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import 'core-js'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'core-js';
 
-import App from './App'
-import store from './store'
+import App from './App';
+import store from './store';
+
+// Get the base URL from the environment variables (for GitHub Pages deployment)
+const basename = process.env.PUBLIC_URL || '/';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
-  </Provider>,
-)
+    <Router basename={basename}>
+      <App />
+    </Router>
+  </Provider>
+);
